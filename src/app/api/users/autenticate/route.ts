0e69ec46c0ae7cf8606427a37f755b88f8228c79
Paulo Junior <juniorpaulo490@gmail.com>
2024-11-23 23:32:06 -0300

    feito endpoint de login com retorno de tokens de autenticacao

---
+++ src/app/api/users/autenticate/route.ts
@@ -0,0 +1,39 @@
+import Users from "@/app/models/usersOrm";
+import { NextResponse } from "next/server";
+import { loginInterface, tokenLoginInterface } from "../interface";
+import bcrypt from "bcrypt";
+import { jwtGenerateToken } from "@/utils/jwtToken";
+
+async function login(req: Request) {
+  const body: loginInterface = await req.json();
+
+  try {
+    const user = await Users.findOne({
+      attributes: ["email", "senha"],
+      where: {
+        email: body.email,
+      },
+    });
+
+    if (user && (await bcrypt.compare(body.senha, user.senha))) {
+      const objResponse: tokenLoginInterface = {
+        accessToken: jwtGenerateToken(user.idUsuario, "1h"),
+        refreshToken: jwtGenerateToken(user.idUsuario, "30d"),
+      };
+
+      return NextResponse.json({ data: objResponse }, { status: 200 });
+    } else {
+     return NextResponse.json(
+        { message: "Usuário ou senha inválidos" },
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Erro ao seguir com o cadastro", error },
+      { status: 500 }
+    );
+  }
+}
+
+export { login as POST };
--- src/app/api/users/interface.ts
+++ src/app/api/users/interface.ts
@@ -1,1 +1,9 @@
+export interface loginInterface {
+  email: string;
+  senha: string;
+}
 
+export interface tokenLoginInterface {
+  accessToken: string;
+  refreshToken: string;
+}
